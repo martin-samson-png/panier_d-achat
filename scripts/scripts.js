8b1583ff7bf9dc95adcfe30210d7b81b031103b6
martin-samson-png <martinsams64@gmail.com>
2025-04-23 15:13:51 +0200

    feat : ajout de la fonction produits

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,19 @@
-const productDiv = document.getElementById("product1");
-const childArray = Array.from(productDiv.children)
-const button_product = document.getElementById("button-product");
-const cartItem = document.getElementById("cartItem")
-button_product.addEventListener("click", () => {
-    const cartDiv = document.createElement("div")
-    cartItem.appendChild(cartDiv)
-    childArray.forEach(element => {
-        const newCartItem = document.createElement("span");
-        newCartItem.textContent = element.textContent;
-        cartDiv.appendChild(newCartItem);
-    })    
-    
-})
+function addProductToCart(productId, buttonId, cartId) {
+    const productDiv = document.getElementById(productId);
+    const childArray = Array.from(productDiv.children);
+    const button_product = document.getElementById(buttonId);
+    const cartItem = document.getElementById(cartId);
+
+    button_product.addEventListener("click", () => {
+        const cartDiv = document.createElement("div");
+        cartItem.appendChild(cartDiv);
+        childArray.forEach(element => {
+            const newCartItem = document.createElement("span");
+            newCartItem.textContent = element.textContent;
+            cartDiv.appendChild(newCartItem);
+        });
+    });
+}
+addProductToCart("product1", "button-product1", "cartItem");
+addProductToCart("product2", "button-product2", "cartItem");
+addProductToCart("product3", "button-product3", "cartItem");
