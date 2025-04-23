0196c0a620f50d075a8572e63eb3f8e072f056c2
martin-samson-png <martinsams64@gmail.com>
2025-04-23 15:55:30 +0200

    feat : finalisation du projet js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,10 @@
+
+function deleteCart(productId){
+    console.log(productId)
+    
+}
+
+
 function addProductToCart(productId, buttonId, cartId) {
     const productDiv = document.getElementById(productId);
     const childArray = Array.from(productDiv.children);
@@ -6,14 +13,29 @@ function addProductToCart(productId, buttonId, cartId) {
 
     button_product.addEventListener("click", () => {
         const cartDiv = document.createElement("div");
+        cartDiv.setAttribute("data-id", productId);
         cartItem.appendChild(cartDiv);
         childArray.forEach(element => {
             const newCartItem = document.createElement("span");
             newCartItem.textContent = element.textContent;
             cartDiv.appendChild(newCartItem);
         });
+        const delete_button = document.createElement("button");
+        delete_button.setAttribute("data-id", productId);
+        delete_button.textContent = "Supprimer";
+        cartDiv.appendChild(delete_button);
+        delete_button.addEventListener("click", () =>{
+            deleteCart(productId)
+            console.log(cartDiv.getAttribute("data-id"))
+            if (cartDiv.getAttribute("data-id") === delete_button.getAttribute("data-id")){
+                cartDiv.remove()
+            }
+        })
     });
 }
 addProductToCart("product1", "button-product1", "cartItem");
 addProductToCart("product2", "button-product2", "cartItem");
 addProductToCart("product3", "button-product3", "cartItem");
+
+
+
