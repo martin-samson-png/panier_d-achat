ab1061144a49a96408cb366ac03d98b51e69d148
fatia2025 <essaidifatiha@gmail.com>
2025-04-23 15:17:04 +0200

    btn supression

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+cart = document.getElementById("cart");
+const button = document.getElementById("button_supp");
+bouton.addEventListener("click", function () {
+  alert("produit suprimer!");
+});
+
+// Supprimer une classe
+box.classList.remove("cart");
+// Alterner une classe
+box.classList.toggle("highlight");
